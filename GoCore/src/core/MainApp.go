ccbbb8b3c3be38d7297a3efd0c242b6c3882abb1
lukaspet26 <89996794+lukaspet26@users.noreply.github.com>
2021-09-27 12:33:17 +0200

    updated

--- GoCore/src/core/MainApp.go
+++ GoCore/src/core/MainApp.go
@@ -7,4 +7,8 @@ func main() {
 	fmt.Println("Go Core Initialize")
 
 	initPostgres()
+	fmt.Printf(DB_NAME)
+	fmt.Printf(DB_USER)
+	fmt.Printf(DB_PASS)
+	fmt.Printf(DB_PORT)
 }
--- GoCore/src/core/PostgresHandler.go
+++ GoCore/src/core/PostgresHandler.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
-)
-
-// import postgres package
-import (
-	"github.com/go-pg/pg"
 )
 
 var DB_NAME,
@@ -28,17 +22,4 @@ func initPostgres() {
 	DB_USER = os.Getenv("DB_USER")
 	DB_PASS = os.Getenv("DB_PASS")
 	DB_PORT = os.Getenv("DB_PORT")
-}
-
-func connectPostgres() {
-	db := pg.Connect(&pg.Options{
-		User:     DB_USER,
-		Password: DB_PASS,
-		Database: DB_NAME,
-	})
-	defer db.Close()
-}
-
-func selectRequests(){
-	fmt.Println("Select requests based on django project")
 }
