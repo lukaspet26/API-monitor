b2eec65fb4c2f61944d3bda6248694b2f1703296
lukaspet26 <89996794+lukaspet26@users.noreply.github.com>
2021-09-30 12:00:26 +0200

    add requests http libraries

--- GoCore/src/core/Requests.go
+++ GoCore/src/core/Requests.go
@@ -5,31 +5,3 @@ import (
 	"io/ioutil"
 	"net/http"
 )
-
-func httpGetRequestWithoutToken(url string) int {
-	// send get request and receive response bytes
-	response, err := http.Get(url)
-
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
-
-
-	bodyBytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	// cast response bytes to json
-	responseCodeInstance := ResponseCode{}
-	err = json.Unmarshal(bodyBytes, &responseCodeInstance)
-	
-	if err != nil {
-		fmt.Println(err)
-		return 700
-	}
-
-	return responseCodeInstance.ResponseCode
-
-}
